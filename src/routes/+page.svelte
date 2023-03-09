39e052c616aa355171f96c8c13988458b77e036e
Jerome Dupret <DJerome99@laposte.net>
2023-03-09 10:25:11 +0100

    REFACTOR!!! Changed from Vite to just svelte. Didnt understand the Vite routing. Some css fixes need. Main Page skeleton added.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    import Logo from '../lib/images/FIG_Logo.png'   
+    import Register from '../comp/Register.svelte';
+  </script>
+  
+  <main>
+    <div id="Main_Logo">
+        <img src={Logo} alt="Oops No Logo"/>
+    </div>
+    <div id="Main_Login">
+      <!-- Register pops up a log in or sign in screen for the user to fill in. Needs a on destroy method when clicked-->
+        <Register />
+        <!-- all buttons, Register and Browsing should take you the the main page. Could add tokne handling in the future -->
+        <nav>
+            <a id="Browsing" href="/Main">Just Browsing</a>
+        </nav>
+    </div>
+  </main>
+  
+  <style>
+    img{
+      width: 20vw;
+      height: max-content;
+      border-radius: 25%;
+    }
+  </style>
+  
---
+++ src/routes/Main/+page.svelte
@@ -0,0 +1,100 @@
+<script>
+    import Logo from '../../lib/images/FIG_Logo.png'
+</script>
+  
+  <div class="header">
+    <div class="header__logo"></div>
+    <div class="header__buttons">
+      <button class="header__button">Button 1</button>
+      <button class="header__button">Button 2</button>
+      <button class="header__button">Button 3</button>
+      <button class="header__button">Button 4</button>
+    </div>
+    <button class="header__cart">Cart</button>
+  </div>
+  
+  <div class="ad-space">
+    Ad space
+  </div>
+  
+  <div class="main">
+    Main section
+  </div>
+  
+  <div class="sidebar">
+    Sidebar section
+  </div>
+  
+
+  <style>
+    /* styles for the header */
+    .header {
+      position: sticky;
+      top: 0;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 10px;
+      background-color: black;
+      color: purple;
+    }
+  
+    .header__logo {
+      width: 50px;
+      height: 50px;
+      background-image: url({logo});
+      background-size: cover;
+    }
+  
+    .header__buttons {
+      display: flex;
+      justify-content: center;
+      gap: 20px;
+    }
+  
+    .header__button {
+      padding: 10px;
+      background-color: purple;
+      color: white;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+    }
+  
+    .header__cart {
+      padding: 10px;
+      background-color: purple;
+      color: white;
+      border: none;
+      border-radius: 50%;
+      cursor: pointer;
+    }
+  
+    /* styles for the ad space */
+    .ad-space {
+      height: 50px;
+      border: 1px solid white;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: black;
+      color: white;
+    }
+  
+    /* styles for the main body */
+    .main {
+      width: 80%;
+      border: 1px solid white;
+      height: 500px;
+      margin: 20px;
+      background-color: black;
+    }
+  
+    .sidebar {
+      width: 20%;
+      border: 1px solid white;
+      height: 500px;
+      margin: 20px;
+      background-color: black;
+    }
+  </style>
